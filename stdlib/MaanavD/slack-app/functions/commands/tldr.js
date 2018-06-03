be74dfd1ac1c2febb3ce5b928dea85d8e5933b04
MaanavD <maanav1@hotmail.com>
2018-06-03 19:34:08 -0400

    we dun boiis almost

--- stdlib/MaanavD/slack-app/functions/commands/tldr.js
+++ stdlib/MaanavD/slack-app/functions/commands/tldr.js
@@ -12,12 +12,18 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
 
-    var token = 'xoxp-371545814469-374465758244-375880975175-7cd41db99d5b4c303d6d681d32adc97a';
+    var token = 'xoxp-371545814469-374465758244-374746016499-37dee26c967e4a59f6a1d5635033e31c';
     var channel = 'CB040QE8G';
+
+    try {
+        var msgNum = parseInt(text);
+    } catch (err) {
+        var msgNum = 1024;
+    }
 
     function httpGet(theUrl, callback) {
         var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() {
+        xmlHttp.onreadystatechange = function () {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                 callback(xmlHttp.responseText);
             }
@@ -28,114 +34,141 @@ module.exports = (user, channel, text = '', command = {}, botToken = null, callb
 
     var out;
 
-    httpGet('https://slack.com/api/channels.history?token=' + token + '&channel=' + channel + '&pretty=1', function(response) {
-        var unfiltered = JSON.parse(response).messages;
-        var message = [];
-        for (let i = 0; i < unfiltered.length; i ++) {
-            if (unfiltered[i].type === "message"
-            && unfiltered[i].subtype != "bot_message"
-            && unfiltered[i].subtype != "channel_join"
-            && unfiltered[i].subtype != "channel_leave"
-            && unfiltered[i].subtype != "group_join"
-            && unfiltered[i].subtype != "group_leave"
-            && unfiltered[i].subtype != "unpinned_item") {
-                message.push(unfiltered[i]);
+    httpGet('https://slack.com/api/users.list?token=' + token + '&pretty=1', function (response) {
+        var memberList = JSON.parse(response).members;
+
+        httpGet('https://slack.com/api/channels.history?token=' + token + '&channel=' + channel + '&pretty=1', function (response) {
+            var unfiltered = JSON.parse(response).messages;
+            var message = [];
+
+            if (msgNum > unfiltered.length) {
+                msgNum = unfiltered.length;
             }
-        }
 
-        var want = new Array(message.length).fill(false);
+            for (let i = 0; i < msgNum; i++) {
+                if (unfiltered[i].type === "message"
+                    && unfiltered[i].subtype != "bot_message"
+                    && unfiltered[i].subtype != "channel_join"
+                    && unfiltered[i].subtype != "channel_leave"
+                    && unfiltered[i].subtype != "group_join"
+                    && unfiltered[i].subtype != "group_leave"
+                    && unfiltered[i].subtype != "unpinned_item") {
+                    message.push(unfiltered[i]);
+                }
+            }
 
-        var keywords = [{"text": "\\?", "after": 3}];
-        byContent(message, keywords);
+            var want = new Array(message.length).fill(false);
 
-        var trange = 5000;
-        byFrequency(message);
+            var keywords = [{ "text": "\\?", "after": 3 }];
+            byContent(message, keywords);
 
-        minLength = 128;
-        byLength(message);
+            var trange = 10000;
+            byFrequency(message);
 
-        minReactions = 3;
-        byReactions(message);
+            var minLength = 128;
+            byLength(message);
 
-        function byContent(a, b) {
-            for (let t = 0; t < b.length; t ++) {
-                for (let i = 0; i < a.length; i ++) {
-                    if (a[i].text.search(b[t].text) != -1) {
-                        for (let j = 0; j <= b[t].after; j ++) {
-                            want[i + j] = true;
+            var stubLength = 32;
+            removeShort(message);
+
+            var minReactions = 3;
+            byReactions(message);
+
+            function byContent(a, b) {
+                for (let t = 0; t < b.length; t++) {
+                    for (let i = 0; i < a.length; i++) {
+                        if (a[i].text.search(b[t].text) != -1) {
+                            for (let j = 0; j <= b[t].after; j++) {
+                                want[i + j] = true;
+                            }
                         }
                     }
                 }
             }
-        }
 
-        function byFrequency(a) {
-            for (let i = 1; i < a.length; i ++) {
-                if ((a[i].ts - a[i - 1].ts) <= trange) {
-                    want[i] = true;
-                    want[i - 1] = true;
-                }
-            }
-        }
-
-        function byLength(a) {
-            for (let i = 0; i < a.length; i ++) {
-                if (a[i].length >= minLength) {
-                    want[i] = true;
-                }
-            }
-        }
-
-        function byReactions(a) {
-            for (let i = 0; i < a.length; i ++) {
-                a[i].reactioncount = 0;
-                if (a[i].reactions != undefined) {
-                    for (let j = 0; j < a[i].reactions.length; j ++) {
-                        a[i].reactioncount += a[i].reactions[j].count;
+            function byFrequency(a) {
+                for (let i = 1; i < a.length; i++) {
+                    if ((a[i].ts - a[i - 1].ts) <= trange) {
+                        want[i] = true;
+                        want[i - 1] = true;
                     }
-                    if (a[i].reactioncount >= minReactions) {
+                }
+            }
+
+            function byLength(a) {
+                for (let i = 0; i < a.length; i++) {
+                    if (a[i].text.length >= minLength) {
                         want[i] = true;
                     }
                 }
             }
-        }
-        
-        function displayuser(id) {
-            var out;
-            httpGet('https://slack.com/api/users.info?token=' + token + '&user=' + id + '&pretty=1', out = function(response) {
-                if (response.user.profile.display_name === "") {
-                    return response.user.profile.real_name;
+
+            function removeShort(a) {
+                for (let i = 0; i < a.length; i++) {
+                    if (a[i].text.length < stubLength) {
+                        want[i] = false;
+                    }
                 }
-                return response.user.profile.display_name;
             }
-            return out;
-        }
 
-        var final = [];
-        for (let i = 0; i < message.length; i ++) {
-            if (want[i]) {
-                final.push(message[i]);
+            function byReactions(a) {
+                for (let i = 0; i < a.length; i++) {
+                    a[i].reactioncount = 0;
+                    if (a[i].reactions != undefined) {
+                        for (let j = 0; j < a[i].reactions.length; j++) {
+                            a[i].reactioncount += a[i].reactions[j].count;
+                        }
+                        if (a[i].reactioncount >= minReactions) {
+                            want[i] = true;
+                        }
+                    }
+                }
             }
-        }
 
-        var attach = [{
-            "color": "#ffe4e1",
-            "pretext": "Here's what I was able to come up with:",
-            "author_name": displayuser(final[0].user),
-            "text": final[0].text
-        }];
+            var final = [];
+            for (let i = 0; i < message.length; i++) {
+                if (want[i]) {
+                    final.push(message[i]);
+                }
+            }
 
-        for (let i = 1; i < message.length; i ++) {
-            attach.unshift({
-                "color": "#ffe4e1",
-                "author_name": displayuser(final[i].user),
-                "text": final[i].text
-            })
-        }
+            function displayname(idNum) {
+                for (let i = 0; i < memberList.length; i++) {
+                    if (memberList[i].id == idNum) {
+                        return memberList[i].real_name;
+                    }
+                }
+                return 'Unnamed User';
+            }
 
-        callback(null, {
-            text: `Hey, <@${user}>, I see you need an update on the past ${text} Messages.\n`,
-            attachments: attach
+            if (final.length > 0) {
+                var attach = [];
+                for (let i = 1; i < final.length - 1; i++) {
+                    attach.unshift({
+                        "color": "#ffe4e1",
+                        "title": displayname(final[i].user),
+                        "text": final[i].text,
+                        "ts": final[i].ts
+                    })
+                }
+
+                attach.unshift({
+                    "color": "#ffe4e1",
+                    "pretext": "Here's what I was able to come up with:",
+                    "title": displayname(final[final.length - 1].user),
+                    "text": final[final.length - 1].text,
+                    "ts": final[final.length - 1].ts
+                });
+                
+                callback(null, {
+                    text: `Hey, <@${user}>, I see you need an update on the past ${text} messages.\n`,
+                    attachments: attach
+                });
+            } else {
+                callback(null, {
+                    text: `Unfortunately it seems like nothing interesting happened in the past ${text} messages.`
+                });
+            }
         });
     });
 };
