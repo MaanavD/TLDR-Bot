2295cdffd9d6e7bb57646f0a67f4cdfd82975ecc
xujustinj <xu.justin.j@gmail.com>
2018-06-03 16:57:18 -0400

    Test Version

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ const {WebClient} = require('@slack/client');
 var SLACK_TOKEN = {
     "token" : "xoxb-371545814469-375812478375-2CzUzshmPET193yXVqp9Lwuq",
     "text" : "Approved! you are a winner!"
-  }
+}
 const token = process.env.SLACK_TOKEN;
-
-
 const web = new WebClient(token);
-
 const conversationId = 'GB06G3ERX';
 
 web.chat.postMessage({ channel: conversationId, text: 'Hello there' })
-  .then((res) => {
+    .then((res) => {
     // `res` contains information about the posted message
-    console.log('Message sent: ', res.ts);
-  })
-  .catch(console.error);
+        console.log('Message sent: ', res.ts);
+    })
+    .catch(console.error);
--- stdlib/MaanavD/slack-app/functions/commands/tldr.js
+++ stdlib/MaanavD/slack-app/functions/commands/tldr.js
@@ -12,55 +12,17 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
 
-<<<<<<< HEAD
-function httpGet(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            callback(xmlHttp.responseText);
-        }
-    }
-    xmlHttp.open("GET", theUrl, true);
-    xmlHttp.send(null);
-}
-var out;
-httpGet('https://slack.com/api/channels.history?token=xoxp-371545814469-374465758244-374965082245-6c3131dbacfb2af317ff6eeea18d0d47&channel=CAXBZULM8&pretty=1', function(message) {out = message; callback(null, {
-    text: `Hey, <@${user}>, I see ${out} you need an update on the past ${text} Messages.\n`,
-    attachments: [
-        {
-            "fallback": "Required plain-text summary of the attachment.",
-            "color": "#ffe4e1",
-            "pretext": "Here's what I was able to come up with:",
-            // "author_name": "Bobby Tables",
-            // "author_link": "http://flickr.com/bobby/",
-            // "author_icon": "http://flickr.com/icons/bobby.jpg",
-            // "title": "Slack API Documentation",
-            // "title_link": "https://api.slack.com/",
-            "text": "test here",
-            // "fields": [
-            //     {
-            //         "title": "Priority",
-            //         "value": "High",
-            //         "short": false
-            //     }
-            // ],
-            // "image_url": "http://my-website.com/path/to/image.jpg",
-            // "thumb_url": "http://example.com/path/to/thumb.png",
-            "footer": "Brought to you by the TLDR Bot Team",
-            // "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-            "ts": Math.round((new Date()).getTime() / 1000)
-=======
     function httpGet(theUrl, callback) {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function() {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                 callback(xmlHttp.responseText);
             }
->>>>>>> ebac8b934ea316717795f834559304b1ff5dc643
         }
         xmlHttp.open("GET", theUrl, true);
         xmlHttp.send(null);
     }
+    
     var out;
 
     httpGet('https://slack.com/api/channels.history?token=xoxp-371545814469-374146134465-374779681874-8c3e46c40c33f834ccc6ffe9d546ae2a&channel=CAXBZULM8&pretty=1', function(message) {
